efb3d6872d255947755f9e19cf7af4d392c9be08
Travis Jette <travisjette@gmail.com>
2016-10-22 15:36:37 -0600

    the contact form is working better now still a work in progress

--- client/templates/contact/contact.js
+++ client/templates/contact/contact.js
@@ -5,32 +5,46 @@ Template.contact.helpers({
 Template.contact.events({
 "submit .contactForm": function(event, template){
 event.preventDefault();
-var firstName= event.target.firstName.value;
-var lastName= event.target.lastName.value;
-var email=event.target.email.value;
-var phone=event.target.phone.value;
-var message=event.target.message.value;
+    var formData = $(event.target).serializeArray();
+
+    console.log('formData',formData);
+
+    var contactObj = {};
+
+    _.each(formData, function(e){
+        console.log(e);
+        contactObj[e.name] = e.value || false;
+
+    })
+    console.log(contactObj);
+    contactObj.submittedOn = new Date();
 
 
-// Contacts.insert({
-//   firstName : firstName,
-//   lastName  : lastName,
-//   email     : email,
-//   phone     : phone,
-//   message   : message 
-// });
- var dataText = "Message from: " + firstName + " " + lastName + "\rEmail: " + email + "\rPhone: " + phone + "\rContent:" + message;
-  
-  Meteor.call('sendEmail', dataText, function(resp,err){
-    if(resp){
-    alert('Email sent.', resp);
-  } else {
-    alert("There was an error", err)
-  }
-  console.log(dataText);
-  });
- //throwAlert is my helper method which creates popup with message
- 
+    swal({
+        title: "Are you sure?",
+        text: "Are you sure that this is correct?",
+        type: "warning",
+        showCancelButton: true,
+        closeOnConfirm: false,
+        confirmButtonText: "Yes, looks good!",
+        confirmButtonColor: "#ec6c62"
+    }, function (isConfirm) {
+        if (isConfirm === true) {
+            swal("Submitted!", "Your Contact Request has been submitted.", "success");
+            emailObj=contactObj;
+
+            emailObj.subject = "Contact Request Form: Travis Jette and Rachelle";
+            emailObj.message = emailObj.firstName + " - <br><br> Thank you for submitting the Contact Request!  We will respond as quick as possible. Thank You <br><br> - Travis and Rachelle<br><br>" + emailObj.messageArea + "<br>" + emailObj.phone;
+            Meteor.call('sendEmail', emailObj)
+        } else {
+            swal("Cancelled", "Your Contact Request has been cancelled", "error");
+        }
+    });
+
+
+
+
+
 
 }
 });
