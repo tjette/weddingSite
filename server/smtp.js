d23a2230232b3a1389b44169623290779dd71abd
Travis Jette <travisjette@gmail.com>
2017-02-16 22:21:00 -0700

    changed some email stuff

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,5 @@
 Meteor.startup(function(){
-process.env.MAIL_URL = 'smtp://postmaster@sandboxeb7c0096528e40fe8a77501aeb51eb7e.mailgun.org:Futbol20@smtp.mailgun.org:587'
-})
+process.env.MAIL_URL = 'smtp://postmaster@mail.rachelleandtravis.com:8fc8d7e879182db93f058a1b5a1ac96c@smtp.mailgun.org:587' 
+});
+
+// 'smtp://postmaster@sandboxeb7c0096528e40fe8a77501aeb51eb7e.mailgun.org:Futbol20@smtp.mailgun.org:587'
