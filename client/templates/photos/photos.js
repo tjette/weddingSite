ec388ef199abac8f4837bc713339eed7a9ee446d
Travis Jette <travisjette@gmail.com>
2017-02-19 13:01:25 -0700

    changes

--- client/templates/photos/photos.js
+++ client/templates/photos/photos.js
@@ -27,38 +27,41 @@ Template.photos.onCreated(function () {
 //  	}
 //  })
 
-//  var token = '13591390.01bf291.28c2c17dbd214e2f8e6aeda1ef08da7b',
+  //var token = '13591390.01bf291.28c2c17dbd214e2f8e6aeda1ef08da7b',
 
-//     username = 'tjette', // rudrastyh - my username :)
-//     num_photos = 4;
- 
-// $.ajax({ // the first ajax request returns the ID of user rudrastyh
-// 	url: 'https://api.instagram.com/v1/users/search',
-// 	dataType: 'jsonp',
-// 	type: 'GET',
-// 	data: {access_token: token, q: username}, // actually it is just the search by username
-// 	success: function(data){
-// 		console.log(data);
-// 		$.ajax({
-// 			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
-// 			dataType: 'jsonp',
-// 			type: 'GET',
-// 			data: {access_token: token, count: num_photos},
-// 			success: function(data2){
-// 				console.log(data2);
-// 				for(x in data2.data){
-// 					$('.instaTags').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');  
-// 				}
-//     			},
-// 			error: function(data2){
-// 				console.log(data2);
-// 			}
-// 		});
-// 	},
-// 	error: function(data){
-// 		console.log(data);
-// 	}
-// });
+    var token = Meteor.settings.InstagramAPI.ACCESS_TOKEN;
+     username = 'tjette', // rudrastyh - my username :)
+     num_photos = 4;
+ console.log(token);
+    console.log(username);
+    console.log(Meteor.settings.InstagramAPI.ACCESS_TOKEN);
+ $.ajax({ // the first ajax request returns the ID of user rudrastyh
+ 	url: 'https://api.instagram.com/v1/users/search',
+ 	dataType: 'jsonp',
+ 	type: 'GET',
+ 	data: {access_token: token, q: username}, // actually it is just the search by username
+ 	success: function(data){
+ 		console.log(data);
+ 		$.ajax({
+ 			url: 'https://api.instagram.com/v1/users/' + data.data[0].id + '/media/recent', // specify the ID of the first found user
+ 			dataType: 'jsonp',
+ 			type: 'GET',
+ 			data: {access_token: token, count: num_photos},
+ 			success: function(data2){
+ 				console.log(data2);
+ 				for(x in data2.data){
+ 					$('.instaTags').append('<li><img src="'+data2.data[x].images.thumbnail.url+'"></li>');
+ 				}
+     			},
+ 			error: function(data2){
+ 				console.log(data2);
+ 			}
+ 		});
+ 	},
+ 	error: function(data){
+ 		console.log(data);
+ 	}
+ });
 });
 
 
