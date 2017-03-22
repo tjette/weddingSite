0cee55e9dadba60cf1b7a1ce9c2fdf6501e7ec8c
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-03-22 06:43:51 -0600

    trying to add jquery to registry page. Still a work in progress.

--- client/templates/registry/registry.js
+++ client/templates/registry/registry.js
@@ -6,6 +6,13 @@ Template.registry.events({
     'click .registryGift': function(event, template){
         event.preventDefault();
         $('#myModalT').modal('show');
+    },
+      "mouseover .gift": function(event, template) {
+      $(this).closest(".registryInfo").find('info').slideDown();
+
+    console.log("mousehover", event);
+    
     }
+  
 
 });
