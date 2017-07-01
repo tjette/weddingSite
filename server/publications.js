52719922f9b588e40c8eefce657fe81175f33f7f
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-01 07:08:06 -0600

    made changes to publications.js

--- server/publications.js
+++ server/publications.js
@@ -1,7 +1,7 @@
 Meteor.publish( 'guests', function() {
   // Define what data to publish to the client (and how) here.
 return Rsvp.find();
-}); 
+});
 
 Meteor.publish('gifts', function(){
 	return Gift.find();
